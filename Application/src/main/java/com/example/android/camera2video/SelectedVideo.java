fef8f2e9e3f1a1f2d5e5bcbcdccb1521c6102702
CodexStorm <manojkarthik11@gmail.com>
2018-01-20 09:11:45 +0530

    pushing

--- Application/src/main/java/com/example/android/camera2video/SelectedVideo.java
+++ Application/src/main/java/com/example/android/camera2video/SelectedVideo.java
@@ -7,7 +7,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.media.AudioManager;
+import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -17,6 +19,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.SeekBar;
 import android.widget.Toast;
@@ -25,8 +28,22 @@ import android.widget.VideoView;
 
 import com.adeel.library.easyFTP;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 /**
  * Created by manoj on 13-11-2017.
@@ -35,7 +52,11 @@ import java.io.InputStream;
 public class SelectedVideo extends Activity {
     VideoView view;AudioManager audioManager;
     private ProgressDialog prg;
+    static int serverResponseCode;
+    String path;
     Button upLoad;
+    Bundle bundle;
+    String itemValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +64,8 @@ public class SelectedVideo extends Activity {
         view = (VideoView)findViewById(R.id.videoView1);
         Bundle bundle = getIntent().getExtras();
         String path=bundle.getString("path");
-        String itemValue = bundle.getString("itemValue");
+        itemValue = bundle.getString("itemValue");
+        path = bundle.getString("path");
         view.setVideoURI(Uri.parse(path+"/"+itemValue ));
         final VideoView videoView = (VideoView) findViewById(R.id.videoView1);
         MediaController mediaController = new MediaController(this);
@@ -142,7 +164,11 @@ public class SelectedVideo extends Activity {
         dialogBuilder.setPositiveButton("Done", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int whichButton) {
                 Constants.IP =(String)edt.getText().toString();
-                up(v);
+                final File dir = SelectedVideo.this.getExternalFilesDir(null);
+               // Constants.VideoFilePath = bundle.getString("path");
+               // String path = bundle.getString("path");
+                Log.d("FilePath",dir.getAbsolutePath()+itemValue);
+                int respnse = upLoad2Server(dir.getAbsolutePath()+itemValue);
             }
         });
         dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -161,6 +187,96 @@ public class SelectedVideo extends Activity {
         async.execute(address,u,p,directory);//Passing arguments to AsyncThread
     }
 
+    public static int upLoad2Server(String sourceFileUri) {
+        String upLoadServerUri = Constants.IP;
+        // String [] string = sourceFileUri;
+        String fileName = sourceFileUri;
+
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        DataInputStream inStream = null;
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+        String responseFromServer = "";
+
+        File sourceFile = new File(sourceFileUri);
+        if (!sourceFile.isFile()) {
+            Log.e("Huzza", "Source File Does not exist");
+            return 0;
+        }
+        try { // open a URL connection to the Servlet
+            FileInputStream fileInputStream = new FileInputStream(sourceFile);
+            URL url = new URL(upLoadServerUri);
+            conn = (HttpURLConnection) url.openConnection(); // Open a HTTP  connection to  the URL
+            conn.setDoInput(true); // Allow Inputs
+            conn.setDoOutput(true); // Allow Outputs
+            conn.setUseCaches(false); // Don't use a Cached Copy
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Connection", "Keep-Alive");
+            conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+            conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+            conn.setRequestProperty("uploaded_file", fileName);
+            dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\""+ fileName + "\"" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available(); // create a buffer of  maximum size
+            Log.i("Huzza", "Initial .available : " + bytesAvailable);
+
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            // read file and write it into form...
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+            while (bytesRead > 0) {
+                dos.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            // send multipart form data necesssary after file data...
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // Responses from the server (code and message)
+            serverResponseCode = conn.getResponseCode();
+            String serverResponseMessage = conn.getResponseMessage();
+
+            Log.i("Upload file to server", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);
+            // close streams
+            Log.i("Upload file to server", fileName + " File is written");
+            fileInputStream.close();
+            dos.flush();
+            dos.close();
+        } catch (MalformedURLException ex) {
+            ex.printStackTrace();
+            Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//this block will give the response of upload link
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(conn
+                    .getInputStream()));
+            String line;
+            while ((line = rd.readLine()) != null) {
+                Log.i("Huzza", "RES Message: " + line);
+            }
+            rd.close();
+        } catch (IOException ioex) {
+            Log.e("Huzza", "error: " + ioex.getMessage(), ioex);
+        }
+        return serverResponseCode;  // like 200 (Ok)
+
+    } // end upLoad2Server
     class uploadTask extends AsyncTask<String, Void, String> {
 
         @Override
@@ -182,8 +298,10 @@ public class SelectedVideo extends Activity {
                     status = ftp.setWorkingDirectory(params[3]); // if User say provided any Destination then Set it , otherwise
                 }
                 // Upload will be stored on Default /root level on server
+                Bitmap b;
                 Bundle bundle = getIntent().getExtras();
                 String itemValue = bundle.getString("itemValue");
+                b = bitmapper();
                 ftp.uploadFile(is, itemValue);
                 //Toast.makeText(SelectedVideo.this,"Successfull",Toast.LENGTH_LONG).show();
                 return new String("Upload Successful");
@@ -200,7 +318,68 @@ public class SelectedVideo extends Activity {
             Toast.makeText(SelectedVideo.this,str,Toast.LENGTH_LONG).show();
         }
     }
+    public Bitmap bitmapper()
+    {
+        final File dir = SelectedVideo.this.getExternalFilesDir(null);
+        String SrcPath=dir.getAbsolutePath();
 
+        MediaMetadataRetriever mediaMetadataRetriever = new MediaMetadataRetriever();
+
+        mediaMetadataRetriever.setDataSource(SrcPath);
+        String METADATA_KEY_DURATION = mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+
+        Bitmap bmpOriginal = mediaMetadataRetriever.getFrameAtTime(0);
+        int bmpVideoHeight = bmpOriginal.getHeight();
+        int bmpVideoWidth = bmpOriginal.getWidth();
+
+        Log.d("LOGTAG", "bmpVideoWidth:'" + bmpVideoWidth + "'  bmpVideoHeight:'" + bmpVideoHeight + "'");
+
+        byte [] lastSavedByteArray = new byte[0];
+
+        float factor = 0.20f;
+        int scaleWidth = (int) ( (float) bmpVideoWidth * factor );
+        int scaleHeight = (int) ( (float) bmpVideoHeight * factor );
+        int max = (int) Long.parseLong(METADATA_KEY_DURATION);
+        for ( int index = 0 ; index < max ; index++ )
+        {
+            bmpOriginal = mediaMetadataRetriever.getFrameAtTime(index * 1000, MediaMetadataRetriever.OPTION_CLOSEST);
+            bmpVideoHeight = bmpOriginal == null ? -1 : bmpOriginal.getHeight();
+            bmpVideoWidth = bmpOriginal == null ? -1 : bmpOriginal.getWidth();
+            int byteCount = bmpOriginal.getWidth() * bmpOriginal.getHeight() * 4;
+            ByteBuffer tmpByteBuffer = ByteBuffer.allocate(byteCount);
+            bmpOriginal.copyPixelsToBuffer(tmpByteBuffer);
+            byte [] tmpByteArray = tmpByteBuffer.array();
+
+            if ( !Arrays.equals(tmpByteArray, lastSavedByteArray))
+            {
+                int quality = 100;
+                String mediaStorageDir=dir.getAbsolutePath();
+                File outputFile = new File(mediaStorageDir , "IMG_" + ( index + 1 )
+                        + "_" + max + "_quality_" + quality + "_w" + scaleWidth + "_h" + scaleHeight + ".png");
+                Log.e("Output Files::>>",""+outputFile);
+                OutputStream outputStream = null;
+                try {
+                    outputStream = new FileOutputStream(outputFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+                Bitmap bmpScaledSize = Bitmap.createScaledBitmap(bmpOriginal, scaleWidth, scaleHeight, false);
+
+                bmpScaledSize.compress(Bitmap.CompressFormat.PNG, quality, outputStream);
+
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                lastSavedByteArray = tmpByteArray;
+            }
+        }
+        mediaMetadataRetriever.release();
+        return bmpOriginal;
+    }
 
 }
 
